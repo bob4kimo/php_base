56508c05c8c03eb198e2f4abdd867958be216d77
Bob Lu <bob4kimo@gmail.com>
2020-04-22 15:25:04 +0800

    Create Base.php

---
+++ Base.php
@@ -0,0 +1,15 @@
+function _log($obj) {
+    echo $obj."<br>";
+}
+
+function _console($obj) {
+    echo "<script>console.log('PHP: " . $obj . "');</script>";
+}
+
+function getPostData() {
+    return file_get_contents("php://input");
+}
+
+function echoPostData() {
+    echo getPostData();
+}
